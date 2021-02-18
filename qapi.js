0e27ec87c32095d16f00303b4df839cd063b37e4
rockriderST100 <78728278+rockriderST100@users.noreply.github.com>
2021-02-18 18:24:09 +0530

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"602e62fa752fdd001f933121",
+				"602e62fa752fdd001f933123"
+			]
+		},
+		"602e62fa752fdd001f933121": {
+			"id": "602e62fa752fdd001f933121",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"602e62fa752fdd001f933123": {
+			"id": "602e62fa752fdd001f933123",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
